ffdfb0953ab6cdf06f0c8d674235ec348870fb65
Vipul Ojha <ojha04696@gmail.com>
2026-07-03 04:03:09 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3);
+// code readability is important
+console.log("Vipul")
+
+let name = " Vipul"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Vipul");
+console.log(typeof undefined);
+console.log(typeof null);
