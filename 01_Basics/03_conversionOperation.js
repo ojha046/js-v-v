538788644582cfdb9d7fc3d5dc7404ceb06e5874
Vipul Ojha <ojha04696@gmail.com>
2026-07-03 05:12:18 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = '33abc'
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNUmber = Number(score)
+//console.log(typeof valueInNUmber);
+//console.log(valueInNUmber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "vipul"
+
+let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+
+//1=> true; 0 => false
+// "" => false
+// "Vipul" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
